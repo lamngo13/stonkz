f45f939beafcf9256c9a0c16c4f30beb2290a875
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2023-08-24 16:35:06 -0400

    still good at sec

--- dbs/twitter.py
+++ dbs/twitter.py
@@ -1,2 +1,20 @@
 import searchtweets
 import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+
+name = config.get('creds', 'name')
+api_key = config.get('creds', 'api_key')
+api_key_secret = config.get('creds', 'api_key_secret')
+bearer_token = config.get('creds', 'bearer_token')
+access_token = config.get('creds', 'access_token')
+access_token_secret = config.get('creds', 'access_token_secret')
+
+bruh = [name, api_key, api_key_secret, bearer_token, access_token, access_token_secret]
+
+for b in bruh:
+    print(type(b))
+    print(str(b))
