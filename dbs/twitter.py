40e425b31cb9f19f545ba1a8858d6f1a3fc0f802
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2023-09-07 16:09:54 -0400

    better secrets - starting twitter

--- dbs/twitter.py
+++ dbs/twitter.py
@@ -1,23 +1,9 @@
 import searchtweets
-import configparser
-
+from searchtweets import ResultStream, collect_results, load_credentials
 import bruh
+#secret file 
 
-
-'''
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-
-
-name = config.get('creds', 'name')
-api_key = config.get('creds', 'api_key')
-api_key_secret = config.get('creds', 'api_key_secret')
-bearer_token = config.get('creds', 'bearer_token')
-access_token = config.get('creds', 'access_token')
-access_token_secret = config.get('creds', 'access_token_secret')
-'''
-
+#SECRETS
 name = bruh.name
 api_key = bruh.api_key
 api_key_secret = bruh.api_key_secret
@@ -26,6 +12,27 @@ access_token = bruh.access_token
 access_token_secret = bruh.access_token_secret
 zbruh = [name, api_key, api_key_secret, bearer_token, access_token, access_token_secret]
 
-for b in zbruh:
-    print(type(b))
-    print(str(b))
+p_search_args = load_credentials("/.twitter_keys.yaml", yaml_key="search_tweets_api")
+
+
+search_args = {
+    "bearer_token": bearer_token,
+}
+
+query = "dog"
+max_results = 100
+
+
+# Perform the historical search and collect results
+#ztweets = collect_results(query=query,
+#                          max_results=500,
+#                          result_stream_args=search_args,
+#                          from_date="2022-01-01",
+#                          to_date="2022-01-02")
+
+tweets = collect_results(query=query, max_results=max_results, result_stream_args=p_search_args)
+
+# Count occurrences of the keyword
+keyword_count = sum(1 for tweet in tweets if "dog" in tweet.all_text.lower())
+
+print(f"The keyword 'dog' was mentioned {keyword_count} times in the search results.")
