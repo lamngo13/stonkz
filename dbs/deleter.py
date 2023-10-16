32c21c1e18a79947754f2f7c4d5a666c5db526c0
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2023-10-16 00:32:30 -0400

    deleted dupes

--- dbs/deleter.py
+++ dbs/deleter.py
@@ -2,24 +2,23 @@ import sqlite3
 
 stocklist = ["RTX", 'NOC', 'GD', 'LDOS', 'KBR', 'BWXT', 'RKLB', 'LMT']
 
-s = 'RTX'
-#ALLONE THIS IS A SINGLETON
+for s in stocklist:
 
-conn = sqlite3.connect(s+'.db')
-cursor = conn.cursor()
+    conn = sqlite3.connect(s+'.db')
+    cursor = conn.cursor()
 
-# Execute the DELETE statement for identical entries
-query = f'''
-        DELETE FROM {s}
-        WHERE rowid NOT IN (
-            SELECT MIN(rowid)
-            FROM {s}
-            GROUP BY date
-            HAVING COUNT(*) > 1
-        )
-    '''
+    # Execute the DELETE statement for identical entries
+    query = f'''
+            DELETE FROM {s}
+            WHERE rowid NOT IN (
+                SELECT MIN(rowid)
+                FROM {s}
+                GROUP BY date
+                HAVING COUNT(*) > 1
+            )
+        '''
 
-cursor.execute(query)
-# Commit the changes and close the connection
-conn.commit()
-conn.close()
+    cursor.execute(query)
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
