7fb84adcc8575b13c1feb6d3b178b6adf7502c82
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2023-10-24 16:26:08 -0400

    more features

--- dbs/ap.py
+++ dbs/ap.py
@@ -1,97 +1,128 @@
-#howdy
-#howdy
+# Import necessary libraries
 from sklearn.ensemble import RandomForestRegressor
 import sqlite3
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-#stocklist
+# List of stock tickers
 stocklist = ["RTX", 'NOC', 'GD', 'LDOS', 'KBR', 'BWXT', 'RKLB', 'LMT']
+storer = []
 
 # Connect to the SQLite database
 conn = sqlite3.connect('ALLONE.db')
 
-# Query to retrieve stock data for a specific date range
-query = '''
-    SELECT date, open, high, low, close, volume, name 
-    FROM ALLONE 
-    WHERE date BETWEEN '2023-10-01' AND '2023-10-20'
-'''
+# Initialize the imputer
+imputer = SimpleImputer(strategy='mean')
 
-# Load data from the database into a pandas DataFrame
-df = pd.read_sql(query, conn)
+# Loop through each stock ticker
+for stock_ticker in stocklist:
+    # Query to retrieve stock data for the current stock ticker within a specific date range
+    query = f'''
+        SELECT date, open, high, low, close, volume 
+        FROM ALLONE 
+        WHERE date BETWEEN '2023-10-01' AND '2023-10-20' AND name = '{stock_ticker}'
+    '''
+
+    # Load data from the database into a pandas DataFrame
+    df = pd.read_sql(query, conn)
+
+    #---------------------END RETRIEVAL FROM DB----------------------------
+    # Extract date features
+    df['date'] = pd.to_datetime(df['date'])
+    df['day'] = df['date'].dt.day
+    df['month'] = df['date'].dt.month
+    df['day_of_week'] = df['date'].dt.dayofweek
+
+    # Create lag features for previous day's closing price
+    df['lag_close_1'] = df['close'].shift(1)
+
+    # Calculate rolling mean and rolling standard deviation
+    #TODO why are we doing this?
+    window = 3
+
+    #close
+    df['close_mean'] = df['close'].rolling(window=window).mean()
+    df['close_std'] = df['close'].rolling(window=window).std()
+
+    #open
+    df['open_mean'] = df['open'].rolling(window=window).mean()
+    df['open_std'] = df['open'].rolling(window=window).std()
+
+    #high
+    df['high_mean'] = df['high'].rolling(window=window).mean()
+    df['high_std'] = df['high'].rolling(window=window).std()
+    
+
+    #low
+    df['low_mean'] = df['low'].rolling(window=window).mean()
+    df['low_std'] = df['low'].rolling(window=window).std()
+
+    #volume
+    df['volume_mean'] = df['volume'].rolling(window=window).mean()
+    df['volume_std'] = df['volume'].rolling(window=window).std()
+
+
+
+    # Drop NaN values
+    df = df.dropna()
+
+    print("yuh")
+    print(df)
+
+    # Split data into features (X) and target variable (y)
+    X = df[['day', 'month', 'day_of_week', 'lag_close_1', 'close_mean', 'close_std', 'open_mean', 'open_std', 'high_mean', 'high_std', 'low_mean', 'low_std', 'volume_mean', 'volume_std']]
+    y = df['close']
+
+    # Impute missing values in features
+    X_imputed = imputer.fit_transform(X)
+
+    # Initialize the Random Forest model
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+    # Train the model using the imputed data
+    model.fit(X_imputed, y)
+
+    # Get the last available row for the current stock
+    latest_stock_data = df.tail(1)
+
+    # Create lag features for the day you want to predict
+    latest_stock_data['lag_close_1'] = latest_stock_data['close'].shift(1)
+
+    # Extract features for prediction
+    stock_features = latest_stock_data[['day', 'month', 'day_of_week', 'lag_close_1', 'close_mean', 'close_std', 'open_mean', 'open_std', 'high_mean', 'high_std', 'low_mean', 'low_std', 'volume_mean', 'volume_std']].values
+
+    # Impute missing values for prediction
+    stock_features_imputed = imputer.transform(stock_features)
+
+    # Make prediction using the model and imputed features
+    stock_prediction = model.predict(stock_features_imputed)
+
+    #print(f'The next stock price for {stock_ticker} will be: {stock_prediction[0]}')
+    storer.append(f'The next stock price for {stock_ticker} will be: {stock_prediction[0]}')
 
 # Close the database connection
 conn.close()
 
-#---------------------END RETRIEVAL FROM DB----------------------------
-# Extract date features
-df['date'] = pd.to_datetime(df['date'])
-df['day'] = df['date'].dt.day
-df['month'] = df['date'].dt.month
-df['day_of_week'] = df['date'].dt.dayofweek
+print("\n")
+for i in storer:
+    print(i)
 
-# Create lag features for the previous day's closing price
-df['lag_close_1'] = df.groupby('name')['close'].shift(1)
+#assuming for 2023-10-23
+#The next stock price for RTX will be: 73.06670181274414
+#The next stock price for NOC will be: 485.91860931396485 !!!!!!!!!!!!!!!!
+#The next stock price for GD will be: 236.46566650390625
+#The next stock price for LDOS will be: 93.00990005493163 !!!!!!!!!!!!!!!!!!!!!!!
+#The next stock price for KBR will be: 59.38069984436035
+#The next stock price for BWXT will be: 76.84980087280273
+#The next stock price for RKLB will be: 4.281900053024292
+#The next stock price for LMT will be: 443.78630584716797
 
-# Calculate rolling mean and rolling standard deviation
-window = 3
-df['rolling_mean'] = df.groupby('name')['close'].rolling(window=window).mean().reset_index(0, drop=True)
-df['rolling_std'] = df.groupby('name')['close'].rolling(window=window).std().reset_index(0, drop=True)
-
-# One-hot encode stock names
-df = pd.get_dummies(df, columns=['name'], prefix='stock')
-
-# Drop NaN values
-df = df.dropna()
-
-# Split data into features (X) and target variable (y)
-X = df[['day', 'month', 'day_of_week', 'lag_close_1', 'rolling_mean', 'rolling_std', 'stock_RTX', 'stock_NOC', 'stock_GD','stock_LDOS', 'stock_KBR', 'stock_BWXT', 'stock_RKLB', 'stock_LMT']]
-y = df['close']
-
-# Split the data into 80% training and 20% testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Initialize the imputer
-imputer = SimpleImputer(strategy='mean')
-
-# Impute missing values in training and testing features
-X_train_imputed = imputer.fit_transform(X_train)
-X_test_imputed = imputer.transform(X_test)
-
-# Initialize the Random Forest model
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-
-# Train the model using the imputed training data
-model.fit(X_train_imputed, y_train)
-
-# Make predictions on the imputed test data
-predictions = model.predict(X_test_imputed)
-
-# Evaluate the model
-from sklearn.metrics import mean_squared_error, r2_score
-
-mse = mean_squared_error(y_test, predictions)
-r2 = r2_score(y_test, predictions)
-
-print(f'Mean Squared Error: {mse}')
-print(f'R-squared: {r2}')
-
-# Predict specific stock ('LMT')
-# Get the last available row for 'LMT'
-latest_lmt_data = df[df['stock_LMT'] == 1].tail(1)
-
-# Create lag features for the day you want to predict
-latest_lmt_data['lag_close_1'] = latest_lmt_data['close'].shift(1)
-
-# Extract features for prediction
-LMT_features = latest_lmt_data[['day', 'month', 'day_of_week', 'lag_close_1', 'rolling_mean', 'rolling_std']].values
-
-# Impute missing values for prediction
-LMT_features_imputed = imputer.transform(LMT_features)
-
-# Make prediction using the model and imputed features
-LMT_prediction = model.predict(LMT_features_imputed)
-
-print(f'The next stock price for LMT will be: {LMT_prediction[0]}')
+#assuming for 2023-10-23 with more features
+#The next stock price for RTX will be: 73.02210166931152
+#The next stock price for NOC will be: 478.9236083984375
+#The next stock price for GD will be: 235.83083068847657
+#The next stock price for LDOS will be: 93.01390022277832
+#The next stock price for KBR will be: 59.46889976501465
+#The next stock price for BWXT will be: 76.81240135192871
+#The next stock price for RKLB will be: 4.249200053215027
+#The next stock price for LMT will be: 443.26680847167967
